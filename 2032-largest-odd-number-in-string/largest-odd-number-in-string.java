42ec394750050131b94a34274b9aea2956fc06a3
Subhashini Sivakumar <146346955+Subhashini0408@users.noreply.github.com>
2026-08-12 13:17:12 +0530

    Time: 1 ms (99.88%) | Memory: 47.1 MB (56.53%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
@@ -4,7 +4,9 @@ class Solution {
      //As the num does not contains any leading zeros , so we just return it directly
      for(int i = num.length() - 1 ; i >= 0 ; i--)
      {
-        if(Character.getNumericValue(num.charAt(i)) % 2 == 1)
+        char ch = num.charAt(i);
+        int n = ch - '0';
+        if(n % 2 == 1)
         {
             return num.substring(0 , i + 1);
         }
