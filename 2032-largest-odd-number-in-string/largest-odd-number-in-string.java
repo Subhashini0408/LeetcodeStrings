9cb86cfa621e5109e114589b12d4e7fbf606d507
Subhashini Sivakumar <146346955+Subhashini0408@users.noreply.github.com>
2026-08-12 13:16:25 +0530

    Time: 2 ms (17.31%) | Memory: 47.3 MB (20.40%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
@@ -1,27 +1,14 @@
 class Solution {
     public String largestOddNumber(String num) {
 
-       int oddind = -1;
-       for(int i = num.length() - 1 ; i >= 0 ; i--)
-       {
-          if(Character.getNumericValue(num.charAt(i)) % 2 == 1)
-          {
-            oddind = i;
-            break;
-          }
-       }
-
-       if(oddind == -1)
-       {
-          return "";
-       }
-       //remove the leading zeros 
-       int j = 0;
-       while(j < oddind && num.charAt(j) == '0')
-       {
-         j++;
-       }
-
-       return num.substring(j , oddind + 1);
+     //As the num does not contains any leading zeros , so we just return it directly
+     for(int i = num.length() - 1 ; i >= 0 ; i--)
+     {
+        if(Character.getNumericValue(num.charAt(i)) % 2 == 1)
+        {
+            return num.substring(0 , i + 1);
+        }
+     }
+      return "";
     }
 }
