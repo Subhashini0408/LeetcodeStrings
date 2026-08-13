6464234e8b89461794526c2c5c6f618ab2f99d83
Subhashini Sivakumar <146346955+subhashini0408@users.noreply.github.com>
2026-08-13 17:41:20 +0530

    Time: 2 ms (56.77%) | Memory: 43.4 MB (58.92%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -5,29 +5,7 @@ class Solution {
        {
           return false;
        }
-       if(s.equals(goal))
-       {
-          return true;
-       }
-       char[] arr1 = s.toCharArray();
-       char first = arr1[0];
-       char last = arr1[arr1.length - 1];
-       char[] arr2 = goal.toCharArray();
 
-       Arrays.sort(arr1);
-       Arrays.sort(arr2);
-
-       if(Arrays.equals(arr1 , arr2))
-       {
-           for(int i = 0 ; i < goal.length() - 1 ; i++)
-           {
-              if(goal.charAt(i) == last && goal.charAt(i + 1) == first)
-              {
-                 return true;
-              }
-           }
-       }
-
-       return false;
+       return (s + s).contains(goal);
     }
 }
