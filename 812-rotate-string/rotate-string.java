b3c76ad3542200b696f89e512c7f08b51f7b4394
Subhashini Sivakumar <146346955+subhashini0408@users.noreply.github.com>
2026-08-13 17:26:51 +0530

    Time: 1 ms (86.80%) | Memory: 43.3 MB (58.92%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,30 +1,29 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
         
-        if(s.equals(goal))
-        {
-            return true;
-        }
-        char[] arr1 = s.toCharArray();
-        char[] arr2 = goal.toCharArray();
+       if(s.equals(goal))
+       {
+          return true;
+       }
+       char[] arr1 = s.toCharArray();
+       char first = arr1[0];
+       char last = arr1[arr1.length - 1];
+       char[] arr2 = goal.toCharArray();
 
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
+       Arrays.sort(arr1);
+       Arrays.sort(arr2);
 
-        if(Arrays.equals(arr1 , arr2))
-        {
-            char[] arr = s.toCharArray();
-            char first = arr[0];
-            char last = arr[arr.length - 1];
-            for(int i = 0 ; i < goal.length() ; i++)
-            {
-               if(goal.charAt(i) == last && goal.charAt(i + 1) == first)
-               {
-                  return true;
-               }
-            }
-        }
+       if(Arrays.equals(arr1 , arr2))
+       {
+           for(int i = 0 ; i < goal.length() - 1 ; i++)
+           {
+              if(goal.charAt(i) == last && goal.charAt(i + 1) == first)
+              {
+                 return true;
+              }
+           }
+       }
 
-        return false;
+       return false;
     }
 }
